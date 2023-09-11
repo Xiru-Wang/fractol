b6d07fd2a3681d1ee5d0c1dd1c1cd558e98aba80
Xiru/Ciru <79924696+Xiru-Wang@users.noreply.github.com>
2023-09-11 23:01:34 +0200

    Update events.c

--- events.c
+++ events.c
@@ -52,33 +52,29 @@ int	mouse_hook(int keycode, int x, int y, t_fractal *fractal)
 	return (0);
 }
 
+
+//The zoom follows the actual mouse position
 /*
 int	mouse_hook(int keycode, int x, int y, t_fractal *fractal)
 {
-	double width = (double)SIZE; // assuming SIZE is 800
-	double height = (double)SIZE;
-
 	// Calculate the offset of the mouse from the center of the view
-	double offsetX = (x - SIZE * 0.5) / 200.0;
-	double offsetY = (y - SIZE * 0.5) / 200.0;
+	double offsetx = (x / RATIO - 2.0);//? z.x?
+	double offsety = (y / RATIO - 2.0);
 
 	if (keycode == ZOOM_IN)
 	{
 		fractal->zoom *= 0.9;
 		
 		// Adjust the view offset based on the mouse position
-		fractal->offset_x += offsetX * (1.0 - 0.9);
-		fractal->offset_y += offsetY * (1.0 - 0.9);
+		fractal->offset_x += offsetx * (1.0 - 0.9);
+		fractal->offset_y += offsety * (1.0 - 0.9);
 	}
 	else if (keycode == ZOOM_OUT)
 	{
 		fractal->zoom *= 1.1;
-
-		// Adjust the view offset based on the mouse position
-		fractal->offset_x -= offsetX * (1.1 - 1.0);
-		fractal->offset_y -= offsetY * (1.1 - 1.0);
+		fractal->offset_x -= offsetx * (1.1 - 1.0);
+		fractal->offset_y -= offsety * (1.1 - 1.0);
 	}
-
 	call_fractal(fractal, fractal->name);
 	return (0);
 }
@@ -86,20 +82,9 @@ int	mouse_hook(int keycode, int x, int y, t_fractal *fractal)
 
 /*
  * TRACK the mouse
-*/ 
-//The zoom follows the actual mouse position. 必须要把int x, y放进struct
+*/
 
 #define MOUSEMOVE 6
-
-mlx_hook(window, 6, PointerMotionMask, mouse_track, fractal);
-
-int	mouse_track(int x, int y, t_fractal *fractal)
-{
-	fractal->x = x/200.0 * fractal->zoom) + fractal->shift_x;
-	fractal->y = y/200.0 * fractal->zoom) + fractal->shift_y;
-	//call_fractal(fractal);	
-}
-
 
 // Julia evolves when moving the mouse around
 mlx_hook(window, 6, PointerMotionMask, julia_track, fractal);
@@ -107,8 +92,8 @@ int	julia_track(int x, int y, t_fractal *fractal)
 {
 	if (!ft_strncmp(fractal->name, "julia", 5))
 	{
-		fractal->julia_x = x/RATIO * fractal->zoom) + fractal->shift_x;
-		fractal->julia_y = y/RATIO * fractal->zoom) + fractal->shift_y;
+		fractal->julia_cx = x/RATIO * fractal->zoom) + fractal->shift_x;
+		fractal->julia_cy = y/RATIO * fractal->zoom) + fractal->shift_y;
 		fractal_render(fractal);
 	}
 	return 0;
